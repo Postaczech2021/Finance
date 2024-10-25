93e4aad84c5c5713dabdb309b2db7fa146557225
friede.petr@post.cz <friede.petr@post.cz>
2024-10-25 12:01:19 +0200

    dhdhdj

--- finance_project/finance_app/forms.py
+++ finance_project/finance_app/forms.py
@@ -16,6 +16,10 @@ class IncomeForm(forms.ModelForm):
                     'category': 'Kategorie',
                 }
 
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['category'].queryset = Category.objects.filter(is_income=True)
+
 class OutcomeForm(forms.ModelForm):
     class Meta:
         model = Outcome
@@ -24,12 +28,20 @@ class OutcomeForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'amount': forms.NumberInput(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
+    
         }
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ['name']
+        fields = ['name','is_income']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_income': forms.CheckboxInput(attrs={'class': 'form-check-input float-end '}),
+            
+        }
+        labels = {
+
+                     'is_income':'Pouze příjmová kategorie',
+            
         }
--- finance_project/finance_app/models.py
+++ finance_project/finance_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True, default='Jiné')
+    is_income = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
