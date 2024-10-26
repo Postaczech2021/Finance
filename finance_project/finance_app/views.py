a18944a79910f50ee0925dfd5a37c5973c11c20a
Petr <friede.petr@post.cz>
2024-10-26 22:15:44 +0200

    base, list, views some little changes

--- finance_project/finance_app/views.py
+++ finance_project/finance_app/views.py
@@ -4,49 +4,52 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Income, Outcome, Category
 from django.db.models import Sum
 from .forms import IncomeForm, OutcomeForm, CategoryForm
-from django.core.cache import cache
 
 def index(request):
     return render(request, 'index.html')
 
+
 def add_income(request):
-    cache.clear()
-    income_all = Income.objects.aggregate(Sum('amount'))['amount__sum'] or 0
-    if request.method == 'POST':
-        form = IncomeForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form = IncomeForm()
-            income_all = Income.objects.aggregate(Sum('amount'))['amount__sum'] or 0
-    else:
+
+    form = IncomeForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
         form = IncomeForm()
-        print(form.fields['category'].queryset)  # Přidáno pro kontrolu
-    return render(request, 'add_income.html', {'form': form,'income_all':income_all})
+
+    income_all = Income.objects.aggregate(Sum('amount'))['amount__sum'] or 0
+
+    return render(request, 'add_income.html', {'form': form, 'income_all': income_all})
+
 
 def add_outcome(request):
-    cache.clear()
-    outcome_all = Outcome.objects.aggregate(Sum('amount'))['amount__sum'] or 0
-    if request.method == 'POST':
-        form = OutcomeForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form =OutcomeForm()
-            outcome_all = Outcome.objects.aggregate(Sum('amount'))['amount__sum'] or 0
-    else:
+    form = OutcomeForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
         form = OutcomeForm()
-    return render(request, 'add_outcome.html', {'form': form,'outcome_all':outcome_all})
+
+    outcome_all = Outcome.objects.aggregate(Sum('amount'))['amount__sum'] or 0
+
+    return render(request, 'add_outcome.html', {'form': form, 'outcome_all': outcome_all})
+
 
 def list_transactions(request):
     incomes = Income.objects.all()
     outcomes = Outcome.objects.all()
+
     total_income = sum(income.amount for income in incomes)
     total_outcome = sum(outcome.amount for outcome in outcomes)
+
     balance = (total_income - total_outcome) / total_income * 100 if total_income else 0
-    return render(request, 'list.html', {
+
+    context = {
         'incomes': incomes,
         'outcomes': outcomes,
         'balance': balance
-    })
+    }
+
+    return render(request, 'list.html', context)
 
 
 def edit_income(request, income_id):
